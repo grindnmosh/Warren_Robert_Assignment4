94c28e92d7f38e1a79eb3ab32ca2962de99203dc
Robert Warren <grindnmosh@gmail.com>
2013-02-22 09:34:48 -0700

    1st string attempted. unsure how to test them yet. 10 more libraries to come along with testing.

--- Warren_Robert_Assignment4.js
+++ Warren_Robert_Assignment4.js
@@ -1,1 +1,40 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+/*String
+1. Does a string follow a 123-456-7890 pattern like a phone number?
+2. Does a string follow an aaa@bbb.ccc pattern like an email address?
+3. Is the string a URL? (Does it start with http: or https:?)
+4. Title-case a string (split into words, then uppercase the first letter of each word)
+5. Given a string that is a list of things separated by a given string, as well as another string separator, 
+	return a string with the first separator changed to the second: "a,b,c" + "," + "/" → "a/b/c".*/
+
+// 1.
+var myString = "602-688-1290"
+var end = myString.indexOf("-"),
+	start = myString.indexOf("-");
+myString.substring(0,end)
+myString.substring(start,end)
+var end = myString.length;
+myString.substring(start,end)
+
+console.log(myString.substring(602-688-1290))
+
+
+/*Number
+6. Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10
+7. Fuzzy-match a number: is the number above or below a number within a certain percent?
+8. Find the number of hours or days difference between two dates.
+9. Given a string version of a number such as "42", return the value as an actual Number, such as 42.*/
+
+
+
+
+
+
+
+
+/*Array
+10. Find the smallest value in an array that is greater than a given number
+11. Find the total value of just the numbers in an array, even if some of the items are not numbers.
+12. Given an array of objects and the name of a key, return the array sorted by the value of that key in each 
+	of the objects: "a" + [{a:2},{a:3},{a:1}] → [{a:1},{a:2},{a:3}].*/
